fad5679929a231bce935fe4f0bd616286d3654dd
Maayan Kestenberg <maayan10.k@gmail.com>
2020-06-13 01:53:22 +0300

    Implemented OAHashTable and ModHash.

--- src/ModHash.java
+++ src/ModHash.java
@@ -1,13 +1,27 @@
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ModHash {
-	
-	public static ModHash GetFunc(int m, long p){
-		// TODO implement random choice of mod function from family
-		return null;
-	}
-	
-	public int Hash(long key) {
-		// TODO implement hash function
-		return 0;
-	}
+
+    private long a, b, p;
+    private int m;
+
+    public ModHash(long a, long b, int m, long p) {
+        assert 1 <= a && a < p && 0 <= b && b < p && m <= p;
+        this.a = a;
+        this.b = b;
+        this.m = m;
+        this.p = p;
+    }
+
+    public static ModHash GetFunc(int m, long p) {
+        // TODO implement random choice of mod function from family
+        ThreadLocalRandom rand = ThreadLocalRandom.current();
+        return new ModHash(rand.nextLong(1, p), rand.nextLong(p), m, p);
+    }
+
+    public int Hash(long key) {
+        // TODO implement hash function
+        return (int) ((a*key + b) % p) % m;
+    }
 }
--- src/OAHashTable.java
+++ src/OAHashTable.java
@@ -1,35 +1,83 @@
+import java.util.HashSet;
 
 public abstract class OAHashTable implements IHashTable {
-	
-	private HashTableElement [] table;
-	
-	public OAHashTable(int m) {
-		this.table = new HashTableElement[m];
-		// TODO add to constructor as needed
-	}
-	
-	
-	@Override
-	public HashTableElement Find(long key) {
-		// TODO implement find
-		return null;
-	}
-	
-	@Override
-	public void Insert(HashTableElement hte) throws TableIsFullException,KeyAlreadyExistsException {
-		// TODO implement insertion	
-	}
-	
-	@Override
-	public void Delete(long key) throws KeyDoesntExistException {
-		// TODO implement deletion
-	}
-	
-	/**
-	 * 
-	 * @param x - the key to hash
-	 * @param i - the index in the probing sequence
-	 * @return the index into the hash table to place the key x
-	 */
-	public abstract int Hash(long x, int i);
+
+    private HashTableElement[] table;
+    private static final HashTableElement deleted = new HashTableElement(-1, -1);
+
+    public OAHashTable(int m) {
+        this.table = new HashTableElement[m];
+    }
+
+
+    private int findIndex(long key) {
+        for (int i = 0; i < table.length; i++) {
+            int ind = Hash(key, i);
+            if (table[ind] == null) {
+                return -1;
+            } else if (table[ind] == deleted) {
+                continue;
+            } else if (table[ind].GetKey() == key) {
+                return ind;
+            }
+        }
+        return -1; // search sequence is full and doesn't contain key.
+    }
+
+    @Override
+    public HashTableElement Find(long key) {
+        // TODO implement find
+        int ind = findIndex(key);
+        if (ind == -1) {
+            return null;
+        }
+        return table[ind];
+    }
+
+    @Override
+    public void Insert(HashTableElement hte) throws TableIsFullException, KeyAlreadyExistsException {
+        long key = hte.GetKey();
+        int firstDeletedIndex;
+        boolean sawDeleted = false;
+        for (int i = 0; i < table.length; i++) {
+            int ind = Hash(key, i);
+            if (table[ind] == null) {
+                table[ind] = hte;
+                return;
+            } else if (table[ind] == deleted) {
+                // we can not immediately insert at this position,
+                // since key can still be already in the table later in the search sequence.
+                if (!sawDeleted) {
+                    firstDeletedIndex = ind;
+                    sawDeleted = true;
+                }
+                break;
+            } else if (table[ind].GetKey() == key) {
+                throw new KeyAlreadyExistsException(hte);
+            }
+        }
+        if (sawDeleted) {
+            table[firstDeletedIndex] = hte;
+        } else {
+            throw new TableIsFullException(hte);
+        }
+
+    }
+
+
+    @Override
+    public void Delete(long key) throws KeyDoesntExistException {
+        int ind = findIndex(key);
+        if (ind == -1) {
+            throw new KeyDoesntExistException(key);
+        }
+        table[ind] = deleted;
+    }
+
+    /**
+     * @param x - the key to hash
+     * @param i - the index in the probing sequence
+     * @return the index into the hash table to place the key x
+     */
+    public abstract int Hash(long x, int i);
 }
