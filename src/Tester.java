75674678cdd03cc904dcb6a7656d72e4c4ce9db0
stonewow <stonewow1@gmail.com>
2020-06-23 18:53:38 +0300

    Added Q4 to doc file;

--- src/Tester.java
+++ src/Tester.java
@@ -1,45 +1,28 @@
 import java.util.Random;
 
-public class Tester {
+public class Tester{
     public static void main(String[] args) throws Exception{
-		Random rand = new Random();
-        int m,p,n;
-        m = 10000019;
-		p = 1000000007;
-        n = (int) (m / 2);
-        OAHashTable table = new DoubleHashTable(m, p);
+        Random rand = new Random();
+        int m = 10000000;
+        int p = 1000000007;
+        ModHash baseHash = ModHash.GetFunc(m, p);
+        ModHash stepHash = ModHash.GetFunc(m - 1, p);
+        int times = 100000000;
+        int x=0;
+        
+        System.out.println("Started AQP");
+        for (int j = 0; j < times; j++)
+            for (int i = 0; i < m; i++)
+                x = (int) Math.floorMod(baseHash.Hash(i)+((i & 1) == 0 ? i*i : -i*i), m);
+                System.out.println(x);
 
-        long[] c = new long[n];
-        for (int i = 0; i < n; i++)
-            c[i] = (100 * i) + rand.nextInt(100);
+        System.out.println("Started Double");
+        for (int j = 0; j < times; j++)
+            for (int i = 0; i < m; i++)
+                x = stepHash.Hash(i);
+                // x = (int) Math.floorMod(baseHash.Hash(i)+((i & 1) == 0 ? i*i : -i*i), m);
 
-        // SET EVERYTHING TO DELETED
-        System.out.println("BEFORE");
-        for (int i = 0; i < m; i++)
-            table.table[i] = table.deleted;
-        System.out.println("AFTER");
 
-        // INSERTING
-        for (int i = 0; i < n; i++) {
-            if (i > 0) System.out.println("Inserting "+i);
-            // if (i > 0) table.debug=true;
-            try {
-                // table.Insert(new HashTableElement(c[i], c[i]));
-                for (int j = 0; j < m; j++)
-                    table.table[i] = OAHashTable.deleted;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        // DELETING
-        for (int i = 0; i < n; i++) {
-            try {
-                table.Delete(c[i]);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
         
         // int m=6; long p = 23;
         // IHashTable[] arr = new IHashTable[] {new LPHashTable(m,p), new QPHashTable(m,p), new AQPHashTable(m,p), new DoubleHashTable(m,p)};
