539f20b57a82939d87512a6c400dd1aea6b6c7fd
stonewow <stonewow1@gmail.com>
2020-06-23 21:36:05 +0300

    Finished the doc file;

--- src/Tester.java
+++ src/Tester.java
@@ -1,25 +1,47 @@
 import java.util.Random;
+import java.time.Duration;
+import java.time.Instant;
+
 
 public class Tester{
     public static void main(String[] args) throws Exception{
         Random rand = new Random();
         int m = 10000000;
         int p = 1000000007;
+		Instant start, finish;
         ModHash baseHash = ModHash.GetFunc(m, p);
         ModHash stepHash = ModHash.GetFunc(m - 1, p);
-        int times = 100000000;
-        int x=0;
+        int times = 50;
+        int x=0, step=0;
         
+        start = Instant.now();
         System.out.println("Started AQP");
         for (int j = 0; j < times; j++)
             for (int i = 0; i < m; i++)
-                x = (int) Math.floorMod(baseHash.Hash(i)+((i & 1) == 0 ? i*i : -i*i), m);
-                System.out.println(x);
+                x = (int) Math.floorMod((long) baseHash.Hash(i)+((i & 1) == 0 ? i*i : -i*i), m);
+        finish = Instant.now();
+        System.out.println("it took "+Duration.between(start, finish).toMillis()/times);
 
-        System.out.println("Started Double");
+
+        System.out.println("Started Double FloorMod");
+        start = Instant.now();
         for (int j = 0; j < times; j++)
-            for (int i = 0; i < m; i++)
-                x = stepHash.Hash(i);
+            for (int i = 0; i < m; i++) {
+                step = stepHash.Hash(i);
+                x = (int) Math.floorMod((long) baseHash.Hash(x)+step, m);
+            }
+        finish = Instant.now();
+        System.out.println("it took "+Duration.between(start, finish).toMillis()/times);
+
+        System.out.println("Started Double Mod");
+        start = Instant.now();
+        for (int j = 0; j < times; j++)
+            for (int i = 0; i < m; i++) {
+                step = stepHash.Hash(i);
+                x = (int) ((long) baseHash.Hash(x)+step) % m;
+            }
+        finish = Instant.now();
+        System.out.println("it took "+Duration.between(start, finish).toMillis()/times);
                 // x = (int) Math.floorMod(baseHash.Hash(i)+((i & 1) == 0 ? i*i : -i*i), m);
 
 
